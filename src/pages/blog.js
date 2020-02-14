aac7c3a6b3e1308cef7f46e732b9d611f2151a8f
Gregory Murphy <murphygreg6@gmail.com>
2020-02-14 10:51:42 -0700

    pull blog posts from mongodb

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,42 @@
 import React, {Component} from 'react';
+const axios = require('axios');
+
 
 export default class blog extends Component {
+
+  componentDidMount() {
+    this.getBlogPosts()
+  }
+
+  getBlogPosts = async () => {
+    try {
+      const response = await axios.get(`https://gregapis.herokuapp.com/blog`);
+      this.setState({
+              blog: await response.data
+            })
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  blogMap = () => {
+    try{
+    var posts=this.state.blog
+          return posts.map((item, i) =>
+                <div key={i} className="blogPost">
+                <div className="blogHeader">
+                    <h4>{this.state.blog[i].title}</h4>
+                </div>
+                <p className="blogParagraph">{this.state.blog[i].post}</p>
+                </div>
+          )
+    }catch (err){}}
     
     render(){
         return(
     <div className="blog">
-                <div className="blogPost">
-        <div className="blogHeader">
-          <h4>Blog 1</h4>
-        </div>
-        <p className="blogParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur vitae nunc sed velit dignissim sodales ut. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Quis varius quam quisque id. Netus et malesuada fames ac. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Malesuada proin libero nunc consequat interdum varius sit. Eu lobortis elementum nibh tellus. Velit sed ullamcorper morbi tincidunt ornare. Felis donec et odio pellentesque diam volutpat commodo sed egestas. Pulvinar pellentesque habitant morbi tristique senectus et netus. Augue ut lectus arcu bibendum. Mattis molestie a iaculis at erat. Consectetur purus ut faucibus pulvinar elementum integer. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Sodales ut etiam sit amet nisl purus in mollis. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Ultrices tincidunt arcu non sodales neque.
-        </p>
-         </div>
-         <div className="blogPost">
-        <div className="blogHeader">
-          <h4>Blog 2</h4>
-        </div>
-        <p className="blogParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur vitae nunc sed velit dignissim sodales ut. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Quis varius quam quisque id. Netus et malesuada fames ac. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Malesuada proin libero nunc consequat interdum varius sit. Eu lobortis elementum nibh tellus. Velit sed ullamcorper morbi tincidunt ornare. Felis donec et odio pellentesque diam volutpat commodo sed egestas. Pulvinar pellentesque habitant morbi tristique senectus et netus. Augue ut lectus arcu bibendum. Mattis molestie a iaculis at erat. Consectetur purus ut faucibus pulvinar elementum integer. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Sodales ut etiam sit amet nisl purus in mollis. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Ultrices tincidunt arcu non sodales neque.
-        </p>
-         </div>
-         <div className="blogPost">
-        <div className="blogHeader">
-          <h4>Blog 3</h4>
-        </div>
-        <p className="blogParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur vitae nunc sed velit dignissim sodales ut. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Quis varius quam quisque id. Netus et malesuada fames ac. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Malesuada proin libero nunc consequat interdum varius sit. Eu lobortis elementum nibh tellus. Velit sed ullamcorper morbi tincidunt ornare. Felis donec et odio pellentesque diam volutpat commodo sed egestas. Pulvinar pellentesque habitant morbi tristique senectus et netus. Augue ut lectus arcu bibendum. Mattis molestie a iaculis at erat. Consectetur purus ut faucibus pulvinar elementum integer. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Sodales ut etiam sit amet nisl purus in mollis. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Ultrices tincidunt arcu non sodales neque.
-        </p>
-         </div>
+      <div className="return"><a href="/" className="returnHome">Return</a></div>
+                {this.blogMap()}
     </div>
         )}
 } 
